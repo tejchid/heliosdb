e6b3f86041e4a2ed0cd8828cf13aef850a63581d
tejjchid <tejchidco@gmail.com>
2026-03-03 19:08:53 -0800

    remove build artifacts from tracking

--- build/CMakeFiles/heliosdb.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for heliosdb.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tejjchid/heliosdb
-# Build directory: /Users/tejjchid/heliosdb/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BasicTest "/Users/tejjchid/heliosdb/build/basic_test")
-set_tests_properties(BasicTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/tejjchid/heliosdb/CMakeLists.txt;28;add_test;/Users/tejjchid/heliosdb/CMakeLists.txt;0;")
